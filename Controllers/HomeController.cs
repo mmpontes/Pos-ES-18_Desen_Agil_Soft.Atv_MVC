02532dac0f315e5061916b15e97a6c55d65730f4
mmpontes <mariompontes@hotmail.com>
2020-05-27 00:59:10 -0300

    Projeto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Aula03_MVC.Models;
+using Aula03_MVC.Models.Domain;
+
+namespace Aula03_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Responde()
+        {
+            var p = new Pessoa();
+            return View(p);
+        }
+
+        public IActionResult Sucesso(Pessoa psso)
+        {
+            var p = new Pessoa();
+            return View(p);
+        }
+        public IActionResult Salva(Pessoa psso)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Sucesso", psso);
+            }
+
+            return View("Responde");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Models.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula03_MVC.Models.Domain
+{
+    public class Pessoa
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(15, 35)]
+        public int Age { get; set; }
+
+        [Required]
+        //[RegularExpression(@"b[A-Z0-9._%-]+@[A-Z0-9.-]+.[A-Z]{2,4}b")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Sexo { get; set; }
+
+        [Required]
+        public string Opniao { get; set; }        
+    }
+}
